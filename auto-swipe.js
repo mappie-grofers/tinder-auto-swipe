1d6a8ae8f636c949ce0973473575195289c1678a
Mahesh Paliwal <mahesh.paliwal@grofers.com>
2024-02-08 23:19:28 +0530

    Tinder auto swipe console script

---
+++ auto-swipe.js
@@ -0,0 +1,13 @@
+const clickLikeButton = () => {
+    // if you inspect element. You will figure out Like text is inside Span.Hidden
+    const allSpanBtns = document.querySelectorAll("span.Hidden")
+    for(const btn of allSpanBtns){
+        if(btn.textContent.includes('Like')){
+            btn.Click()
+        }
+    }
+    // similarly other filters can be added to check for interests, hobbies, number of pics etc
+}
+
+//call at 200ms interval
+setInterval(clickLikeButton, 200)
